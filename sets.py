0c83c4be65d51298d6380ba9e98deb8975e7ee1a
medreen <medreenwchege@gmail.com>
2025-10-27 15:42:09 +0300

    updated sets

---
+++ sets.py
@@ -0,0 +1,14 @@
+numbers = {1, 2, 3, 4}
+print(numbers)
+print(type(numbers))
+
+letters = {'k', 'l', 'u', 'm'}
+print('k' in letters)
+print('y' in letters)
+
+numbers.add('Mexico')
+print(numbers)
+
+numbers.update([4, 'uli'])
+print(numbers)
+
