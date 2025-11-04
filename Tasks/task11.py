cf4c65d31800d19a5672b52f68eee9b228f4eb68
medreen.wc <medreenwchege@gmail.com>
2025-11-05 02:50:26 +0300

    added changes to 11

--- Tasks/task11.py
+++ Tasks/task11.py
@@ -5,14 +5,22 @@ today = datetime.datetime.now()
 birthday = list(input("Enter a date(yyyy, mm, dd): "))
 birthday = ' '.join(birthday).replace(' ', '').replace(',', ' ')
 
-year = int(birthday[0:5])
-month = int(birthday[5:8])
-day = int(birthday[8:10])
+if len(birthday) == 10:
+    year = int(birthday[0:5])
+    month = int(birthday[5:8])
+    day = int(birthday[8:10])
 
-user_birthday = datetime.datetime(year, month, day)
+    user_birthday = datetime.datetime(year, month, day)
+    age = today.year - user_birthday.year
+    print(age)
 
-age = today.year - user_birthday.year
-print(age)
+else:
+    print('Enter valid date!')
+
+
+ 
+
+
 
 
 
