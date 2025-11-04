f03284ae37ffe9a2b9b0eaceaa00da2c57ecab9c
medreen.wc <medreenwchege@gmail.com>
2025-11-05 01:51:16 +0300

    changes on task

--- Tasks/task11.py
+++ Tasks/task11.py
@@ -1,5 +1,18 @@
-dob = int(input('Enter your date-of-birth (yy-mm-dd): '))
+import datetime
 
-today = 2025-11-2
-age = today - dob
+today = datetime.datetime.now()
+
+birthday = list(input("Enter a date(yyyy, mm, dd): "))
+birthday = ' '.join(birthday).replace(' ', '').replace(',', ' ')
+
+year = int(birthday[0:5])
+month = int(birthday[5:8])
+day = int(birthday[8:10])
+
+user_birthday = datetime.datetime(year, month, day)
+
+age = today.year - user_birthday.year
 print(age)
+
+
+
