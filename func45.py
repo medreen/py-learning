3461eb054111eb8d7214797f2633dd12982680b0
medreen.wc <medreenwchege@gmail.com>
2025-11-06 19:49:28 +0300

    fixed func45

--- func45.py
+++ func45.py
@@ -1,23 +1,19 @@
-def reverse_string(string):
-    string = list(string)
-    string.sort(reverse = True)
-    string = ' '.join(string).replace(' ', '')
-    string = str(string)
-    return (string)
+string = input('Enter a string: ')
 
-revr = reverse_string('eat')
+def reverse_string(string):    
+    return string[:: -1]
+
+revr = reverse_string(string)
 print(revr)
 
 lst = [1, 2, 3, 4, 5, 6]
 
-def get_even(list1):
+def get_even(lst):
     results = []
-    for i in list1:        
+    for i in lst:        
         if i % 2 == 0:
-            return results.append[i]
-        else:
-            return results.append[i]
-        
+         results.append(i)   
+
     return results
 
 evn = get_even(lst)
@@ -25,7 +21,7 @@ print(evn)
 
 square = []
 def get_squares():
-    for j in range(1, 30):
+    for j in range(1, 31):
         x = j ** 2
         square.append(x)
     return square
